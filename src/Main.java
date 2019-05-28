9e2c09821953e617a714b34653d56a0c43e0d51f
nklppc <neekolapapic@gmail.com>
2019-05-28 22:25:11 +0200

    Drugi komit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,7 +12,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        Button b =  new Button();
+        VBox layout = new VBox();
+        layout.getChildren().add(b);
+        Scene s = new Scene(layout,300,300);
 
+        primaryStage.setScene(s);
         primaryStage.show();
     }
 }
