ec30062bd723a801698bb0640fa0abdefb617063
Stefan <stefan.kandic@yahoo.com>
2019-06-21 22:15:06 +0200

    srecan prvi controller svima koji slave

---
+++ src/MainController.java
@@ -0,0 +1,22 @@
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class MainController {
+
+
+    public ImageView trenutnaSlika;
+    public ImageView slikaProizvoda1, slikaProizvoda2, slikaProizvoda3;
+
+    public ImageView preporucenSlika1,preporucenSlika2,preporucenSlika3;
+
+    public Button logo;
+
+    public void logopressed(){
+
+        logo.setText("SUKURAC");
+
+    }
+
+
+
+}
