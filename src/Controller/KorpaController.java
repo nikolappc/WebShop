f195ab811d6a805ea3f65e12196f81667cfcbad4
Stefan <stefan.kandic@yahoo.com>
2019-06-23 21:45:34 +0200

    popravio Prijavu

--- src/Controller/KorpaController.java
+++ src/Controller/KorpaController.java
@@ -3,9 +3,12 @@ package Controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import View.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
 
@@ -29,21 +32,51 @@ public class KorpaController implements Initializable {
     @FXML
     private SplitPane splitPane1;
 
-    @FXML
-    void traziPritisnut(ActionEvent event) {
+    public void pritisnutLogo() {
+        /** Korisnik pritisnuo LOGO*/
 
+        try{
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("..\\FXML\\glavni.fxml"));
+            Parent root = (Parent) loader.load();
+
+            MainController pc = loader.getController();
+
+            Main.scene.setRoot(root);
+
+        }catch (Exception ex){ ex.printStackTrace();}
     }
 
-    @FXML
-    void nalogPritisnut(ActionEvent event) {
 
+    public void traziPritisnut() {
+        /** Korisnik pritisnuo pretragu za proizvode */
+
+        try{
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("..\\FXML\\Katalog.fxml"));
+            Parent root = (Parent) loader.load();
+
+            KatalogController pc = loader.getController();
+            pc.prikaziSve();
+
+            Main.scene.setRoot(root);
+        }catch (Exception ex){}
     }
 
-    @FXML
-    void pritisnutLogo(ActionEvent event) {
+    public void nalogPritisnut() {
+        /** Korisnik pritisnuo dugme za pregled svog naloga */
 
+        try{
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("..\\FXML\\Prijava.fxml"));
+            Parent root = (Parent) loader.load();
+
+            PrijavaController pc = loader.getController();
+
+            Main.scene.setRoot(root);
+
+        }catch (Exception ex){ ex.printStackTrace();}
     }
-
 
 
 	@Override
