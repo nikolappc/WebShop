821af650953c6299ac0f86b038d0f59b6fb9e2a0
MijatMiletic <mijat.miletic@uns.ac.rs>
2019-07-02 16:32:43 +0200

    ajde ppc

---
+++ src/Tests/WebShopTest.java
@@ -0,0 +1,18 @@
+package Tests;
+
+import org.junit.Test;
+
+public class WebShopTest {
+    @Test
+    public void testPrijava(){}
+
+    @Test
+    public void testRegistracija(){}
+
+    @Test
+    public void testNarucivanje(){}
+
+    @Test
+    public void testPretragaNecega(){}
+
+}
